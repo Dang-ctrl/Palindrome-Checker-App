f5c1ad997aed0e157a03b88236316f80c8ebfe8e
Dang-ctrl <viditdang9@gmail.com>
2026-02-27 16:25:52 +0530

    git reset --soft HEAD~1

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,35 @@
-public class PalindromeCheckerApp {
-
+public class Main {
     public static void main(String[] args) {
 
-        // Hardcoded string
-        String word = "madam";
+        // UC1: Application Entry & Welcome Message
+        System.out.println("Welcome to Palindrome Checker App");
+
+        // UC2: Print a Hardcoded Palindrome Result
+        String word = "MADAM";
         String reverse = "";
 
-        // Reverse the string
         for (int i = word.length() - 1; i >= 0; i--) {
             reverse = reverse + word.charAt(i);
         }
 
-        // Check palindrome condition
         if (word.equals(reverse)) {
             System.out.println(word + " is a Palindrome");
         } else {
             System.out.println(word + " is NOT a Palindrome");
+        }
+
+        // UC3: Palindrome Check Using String Reverse
+        String original = "level";
+        String reversed = "";
+
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed = reversed + original.charAt(i);
+        }
+
+        if (original.equals(reversed)) {
+            System.out.println(original + " is a Palindrome");
+        } else {
+            System.out.println(original + " is NOT a Palindrome");
         }
     }
 }
